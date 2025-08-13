1fbfa5d5151152a2c90e2fb6263517e4e2ed93c1
Fiantso Harena <fiantsorav@gmail.com>
2025-08-13 09:25:32 +0300

    feat(api): get all todos

--- src/api/controllers/todo.controller.js
+++ src/api/controllers/todo.controller.js
@@ -1,4 +1,13 @@
-const getAllTodo = () => { }
+import fs from "fs";
+
+const getAllTodo = (_req, res) => {
+  try {
+    const data = fs.readFileSync("src/api/data/todos.json")
+    res.json(JSON.parse(data));
+  } catch (error) {
+    console.error("Error reading file", error);
+  }
+}
 
 const createTodo = () => { }
 
